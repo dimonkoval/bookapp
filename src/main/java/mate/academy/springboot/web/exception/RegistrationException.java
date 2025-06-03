61d6d779830ac96b4b60504c98f5465b4867b888
Dmitriy.Koval <dimonkoval@gmail.com>
2025-06-03 13:05:05 +0300

    Revert "Revert changes from commit Added User"
    This reverts commit 733ed70d74fb5c58a57bf571b6bfa582a8e1c6fc.

---
+++ src/main/java/mate/academy/springboot/web/exception/RegistrationException.java
@@ -0,0 +1,8 @@
+package mate.academy.springboot.web.exception;
+
+public class RegistrationException extends Exception {
+    public RegistrationException(String message) {
+        super(message);
+    }
+}
+
---
+++ src/main/java/mate/academy/springboot/web/service/impl/UserServiceImpl.java
@@ -0,0 +1,32 @@
+package mate.academy.springboot.web.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import mate.academy.springboot.web.dto.UserRegistrationRequestDto;
+import mate.academy.springboot.web.dto.UserResponseDto;
+import mate.academy.springboot.web.exception.RegistrationException;
+import mate.academy.springboot.web.mapper.UserMapper;
+import mate.academy.springboot.web.model.User;
+import mate.academy.springboot.web.repository.UserRepository;
+import mate.academy.springboot.web.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    @Override
+    public UserResponseDto register(UserRegistrationRequestDto request)
+            throws RegistrationException {
+        if (userRepository.existsByEmail(request.getEmail())) {
+            throw new RegistrationException("Email already in use");
+        }
+
+        User user = userMapper.toEntity(request);
+        User saved = userRepository.save(user);
+        return userMapper.toDto(saved);
+    }
+}
+
